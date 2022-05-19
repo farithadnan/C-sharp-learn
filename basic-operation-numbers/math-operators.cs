7a7c0fc325572808c285a42608dd1ee11aa3f52f
Farith Adnan <ayiedfarith@gmail.com>
2022-05-19 15:29:25 +0800

    added ? operators

--- basic-operation-numbers/math-operators.cs
+++ basic-operation-numbers/math-operators.cs
@@ -12,3 +12,26 @@ Console.WriteLine("Product: " + product);
 Console.WriteLine("Quotient: " + quotient);
 Console.WriteLine("Quotient2: " + quotient2);
 Console.WriteLine("Modulus of 7 / 5 : " + (7 % 5));
+
+
+// ?: nullable types
+int? nullableInt = null;
+
+// ?: ternary conditional operator
+// if temp less than 20, it's cold, else it's perfect
+string GetWeatherDisplay(double tempInCelsius) => tempInCelsius < 20.0 ? "Cold." : "Perfect!";
+
+Console.WriteLine(GetWeatherDisplay(15));  // output: Cold.
+Console.WriteLine(GetWeatherDisplay(27));  // output: Perfect!
+
+// ?? null coalescing operator
+// returns the value of its left-hand operand if it isn't null; otherwise, it evaluates the right-hand operand and returns its result
+int bornYear = 1997;
+int result = bornyear ?? bornYear + 1;
+
+// ??= null coalescing assignment operator
+// if the left-hand operand is null, it assigns the value of the right-hand operand to the left-hand operand
+List<int> numbers = null;
+
+(numbers ??= new List<int>()).Add(5);
+Console.WriteLine(string.Join(" ", numbers));  // output: 5
